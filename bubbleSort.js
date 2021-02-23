7c42f4284d9082f6c0fd0e77d3508dd7d73ef52d
Jon Smith <aardvarkjon94@yahoo.com>
2021-02-22 23:03:06 -0600

    Re-did the algorithm once more and added comments.

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,18 +1,27 @@
 const bubbleSort = (arr) => {
     let length = arr.length;
+    //going through each position
     for(let position = 0; position < length; position++){
+        //checking each number
         for(let number = 0; number < length; number++){
-            if(arr[number] > arr[number+1]){
-                let smallerNumber = arr[number];
-                arr[number] = arr[number + 1];
-                arr[number + 1] = smallerNumber;
+            //check if current number is bigger than the next number
+            if(arr[number] > arr[number + 1]){
+                //Save the smaller number
+                let smallerNumber = arr[number + 1];
+                //swap the numbers
+                arr[number + 1] = arr[number];
+                //set the current position as the smaller number
+                arr[number] = smallerNumber;
             }
         }
     }
+    //return the sorted array
     return arr;
 }
 
 
 let testArr = [5, 7, 8, 3, 1, 27];
+let testTwo = [42, 72, 555, 12345678, 1000]
 
 console.log("Sorted: " + bubbleSort(testArr));
+console.log("Sorted: " + bubbleSort(testTwo));
