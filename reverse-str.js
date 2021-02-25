2cb12a0223fb8d127cb03144497d9c4aad9ad568
Jon Smith <aardvarkjon94@yahoo.com>
2021-02-24 22:12:19 -0600

    Simplified code

--- reverse-str.js
+++ reverse-str.js
@@ -2,19 +2,17 @@
 let answer = "";
 
 let reverse_string = (str) => {
-    //split the string into an array of letters
-    let strSplit = str.split("");
     //Create a host array to push letters into
     let newArr = [];
     //Get the length of the split string so I can iterate through it 
-    let length = strSplit.length;
+    let length = str.length;
     //Loop through the string, starting at the end
     for(let letter = length; letter >= 0; letter-- ){
         //Add each letter to the empty array
-        newArr.push(strSplit[letter])
-        //Set the array into a string
-        answer = newArr.join("")
+        newArr.push(str[letter])
+        
     }
+    let answer = newArr.join("");
     //return the answer
    return answer;
 }
